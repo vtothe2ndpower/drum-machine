406f1ee4f0495e9874daba2f0a33434822ce08ee
Victor Vargas <vvargasnunez@gmail.com>
2021-01-15 17:03:17 -0800

    Initial clean up and setup for application

---
+++ src/components/DrumPanel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DrumPanel extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Drum Panel Component</h2>
+        
+      </div>
+    )
+  }
+}
+
+export default DrumPanel;
